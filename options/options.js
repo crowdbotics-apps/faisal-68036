fcb2312e19a900dd535c0e601066dd6ed24a6859
Crowdbotics <team@crowdbotics.com>
2025-11-10 20:18:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "faisal-68036",
+  url: "https://faisal-68036.botics.co",
+  api: "https://faisal-68036.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
